e9fb86f2acd8dbc2aac297d24e715a150cfc05de
Memphis Marshall <memphism2003@gmail.com>
2026-05-16 14:53:51 +0800

    Allow cleanup after exit

--- devstart.sh
+++ devstart.sh
@@ -2,4 +2,7 @@ set -e
 source ./.env
 hadolint services/backend/dev.dockerfile
 hadolint services/frontend/dev.dockerfile
-sudo podman compose -f compose-dev.yml up --build
+# Allow for cleanup after we exit
+set +e
+sudo podman compose -f compose-dev.yml up --build --abort-on-container-exit
+sudo podman compose -f compose-dev.yml down
--- prodtest.sh
+++ prodtest.sh
@@ -1,4 +1,6 @@
 set -e
 sudo podman pull docker-daemon:aurillium/amethyst-backend:latest
 sudo podman pull docker-daemon:aurillium/amethyst-frontend:latest
+set +e
 sudo podman compose -f compose-prod.yml up
+sudo podman compose -f compose-dev.yml down
